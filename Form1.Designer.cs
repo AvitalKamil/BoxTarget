182f4911a757dde71a5180841f8d1c9a5a1b198c
AvitalX Kamil <avitalx.kamil@intel.com>
2020-10-25 11:18:42 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace APPPPPPP
+{
+    partial class BoxTarget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoxTarget));
+            this.btnTarget = new System.Windows.Forms.Button();
+            this.TxtBoxID = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TxtTarget = new System.Windows.Forms.TextBox();
+            this.SqlError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.UpdatedV = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SqlError)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpdatedV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnTarget.Location = new System.Drawing.Point(0, 149);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(364, 59);
+            this.btnTarget.TabIndex = 0;
+            this.btnTarget.Text = "Get Target";
+            this.btnTarget.UseVisualStyleBackColor = true;
+            this.btnTarget.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtBoxID
+            // 
+            this.TxtBoxID.Location = new System.Drawing.Point(68, 3);
+            this.TxtBoxID.Name = "TxtBoxID";
+            this.TxtBoxID.Size = new System.Drawing.Size(222, 22);
+            this.TxtBoxID.TabIndex = 1;
+            this.TxtBoxID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtBoxID_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(364, 55);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Box ID";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.TxtBoxID, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 18);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(358, 34);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(364, 58);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Target";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.TxtTarget, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 18);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 37);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // TxtTarget
+            // 
+            this.TxtTarget.Location = new System.Drawing.Point(64, 3);
+            this.TxtTarget.Name = "TxtTarget";
+            this.TxtTarget.Size = new System.Drawing.Size(230, 22);
+            this.TxtTarget.TabIndex = 2;
+            this.TxtTarget.TextChanged += new System.EventHandler(this.TxtTarget_TextChanged);
+            // 
+            // SqlError
+            // 
+            this.SqlError.ContainerControl = this;
+            // 
+            // UpdatedV
+            // 
+            this.UpdatedV.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            this.UpdatedV.ContainerControl = this;
+            this.UpdatedV.Icon = ((System.Drawing.Icon)(resources.GetObject("UpdatedV.Icon")));
+            // 
+            // BoxTarget
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(364, 208);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnTarget);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BoxTarget";
+            this.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Train/Test";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SqlError)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpdatedV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+ 
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTarget;
+        private System.Windows.Forms.TextBox TxtBoxID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ErrorProvider SqlError;
+        private System.Windows.Forms.ErrorProvider UpdatedV;
+        private System.Windows.Forms.TextBox TxtTarget;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace APPPPPPP
+{
+    public partial class BoxTarget : Form
+    {
+        public BoxTarget()
+        {
+            InitializeComponent();
+        }
+        string LastTarget = "";
+        string connection = "Server = sql1110-lc-in.ger.corp.intel.com,3181; Database = DC_PreProd_Box_Validation; Uid = DC_PreProd_Box_Valid_rw; Password = wPyL3W1PcPe2pKs";
+        SqlCommand command;
+        string sql = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdatedV.Clear();
+            SqlError.Clear();
+            SqlDataReader dataReader;
+            sql = $"SELECT  * FROM [DC_PreProd_Box_Validation].[dbo].[tblBox] where BoxID = {TxtBoxID.Text} ";
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(connection);
+                sqlConnection.Open();
+                command = new SqlCommand(sql, sqlConnection);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    if (TxtTarget.Text == LastTarget)
+                    {
+                        LastTarget = (string)dataReader.GetValue(5);
+                        TxtTarget.Text = (string)dataReader.GetValue(5);
+                    }
+                    else
+                    {
+                        var IsYes = MessageBox.Show($"Do you want to change the target to: {TxtTarget.Text} ?", "Changing target", MessageBoxButtons.YesNo);
+                        if (IsYes == DialogResult.Yes)
+                        {
+                            Update_Target();
+                            LastTarget = TxtTarget.Text;
+                        }
+                        else
+                        {
+                            TxtTarget.Text = (string)dataReader.GetValue(5);
+                        }
+                    }
+                }
+                if (!dataReader.HasRows)
+                {
+                    LastTarget = "";
+                    TxtTarget.Clear();
+                    SqlError.SetError(TxtBoxID, "Box not found");
+                }
+                dataReader.Close();
+                command.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                SqlError.SetError(TxtBoxID, ex.Message);
+            }
+        }
+            private void Update_Target()
+            {
+                try
+                {
+                    SqlError.Clear();
+                    SqlConnection sqlConnection = new SqlConnection(connection);
+                    string query = "UPDATE [dbo].[tblBox] " +
+                                    $" SET Target= '{TxtTarget.Text}'" +
+                                    $" WHERE  boxid = {TxtBoxID.Text}";
+                    SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                    cmd.Parameters.Add("Target", SqlDbType.VarChar).Value = TxtTarget.Text;
+                    sqlConnection.Open();
+                    cmd.ExecuteNonQuery();
+                    sqlConnection.Close();
+                    UpdatedV.SetError(TxtTarget, "Target Updated!");
+                }
+                catch (Exception ex)
+                {
+                    SqlError.SetError(TxtBoxID, ex.Message);
+                }
+            } 
+            private void TxtBoxID_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+            private void TxtTarget_TextChanged(object sender, EventArgs e)
+            {
+                UpdatedV.Clear();
+            }
+    }
+    }
+
+
+
